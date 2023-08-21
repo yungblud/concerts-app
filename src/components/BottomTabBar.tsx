788264cc45405fd52bd8b59814bc8e395b809c5c
yungblud <killi8n@gmail.com>
2023-08-21 22:44:50 +0900

    feat: implemented safe area inset

--- src/components/BottomTabBar.tsx
+++ src/components/BottomTabBar.tsx
@@ -2,18 +2,24 @@ import {View, TouchableOpacity, Text} from 'react-native';
 import type {BottomTabBarProps} from '@react-navigation/bottom-tabs';
 import React, {memo} from 'react';
 import ReactNativeHapticFeedback from 'react-native-haptic-feedback';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 interface Props extends BottomTabBarProps {}
 
 const BottomTabBar = ({state}: Props) => {
+  const {bottom: bottomInset} = useSafeAreaInsets();
   return (
-    <View style={{flexDirection: 'row'}}>
+    <View
+      style={{
+        flexDirection: 'row',
+        backgroundColor: 'orange',
+      }}>
       {state.routes.map(route => {
         return (
           <TouchableOpacity
             onPress={() => ReactNativeHapticFeedback.trigger('impactLight', {})}
             key={route.key}
-            style={{backgroundColor: 'orange', flex: 1, height: 40}}>
+            style={{flex: 1, height: 20 + bottomInset}}>
             <Text>{route.name}</Text>
           </TouchableOpacity>
         );
