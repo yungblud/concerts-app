31a1750c9336ca89a760314d1542fcc6f4ddb4d9
yungblud <killi8n@gmail.com>
2023-08-26 14:05:14 +0900

    feat: modified with memo

--- src/components/BottomTabBar.tsx
+++ src/components/BottomTabBar.tsx
@@ -1,5 +1,5 @@
 import type {BottomTabBarProps} from '@react-navigation/bottom-tabs';
-import React, {memo} from 'react';
+import React, {memo, useMemo} from 'react';
 import ReactNativeHapticFeedback from 'react-native-haptic-feedback';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import styled from 'styled-components/native';
@@ -8,49 +8,48 @@ interface Props extends BottomTabBarProps {}
 
 const BottomTabBar = ({state, navigation}: Props) => {
   const {bottom: bottomInset} = useSafeAreaInsets();
-  console.log('bottom tab bar');
-  return (
-    <Wrapper>
-      {state.routes.map((route, index) => {
-        const isFocused = state.index === index;
-        const onPress = () => {
-          const event = navigation.emit({
-            type: 'tabPress',
-            target: route.key,
-            canPreventDefault: true,
-          });
+  const tabBarItems = useMemo(() => {
+    return state.routes.map((route, index) => {
+      const isFocused = state.index === index;
+      const onPress = () => {
+        const event = navigation.emit({
+          type: 'tabPress',
+          target: route.key,
+          canPreventDefault: true,
+        });
 
-          if (!isFocused && !event.defaultPrevented) {
-            // The `merge: true` option makes sure that the params inside the tab screen are preserved
-            navigation.navigate({name: route.name, merge: true} as any);
-          }
-          ReactNativeHapticFeedback.trigger('impactLight', {});
-        };
-        const onLongPress = () => {
-          navigation.emit({
-            type: 'tabLongPress',
-            target: route.key,
-          });
-          ReactNativeHapticFeedback.trigger('impactLight', {});
-        };
-        return (
-          <TabBarItem
-            accessibilityRole="button"
-            accessibilityState={isFocused ? {selected: true} : {}}
-            onPress={onPress}
-            onLongPress={onLongPress}
-            key={route.key}
-            style={{
-              height: 50 + bottomInset,
-            }}>
-            <TabBarLabel style={{marginBottom: bottomInset}}>
-              {route.name}
-            </TabBarLabel>
-          </TabBarItem>
-        );
-      })}
-    </Wrapper>
-  );
+        if (!isFocused && !event.defaultPrevented) {
+          // The `merge: true` option makes sure that the params inside the tab screen are preserved
+          navigation.navigate({name: route.name, merge: true} as any);
+        }
+        ReactNativeHapticFeedback.trigger('impactLight', {});
+      };
+      const onLongPress = () => {
+        navigation.emit({
+          type: 'tabLongPress',
+          target: route.key,
+        });
+        ReactNativeHapticFeedback.trigger('impactLight', {});
+      };
+      return (
+        <TabBarItem
+          accessibilityRole="button"
+          accessibilityState={isFocused ? {selected: true} : {}}
+          onPress={onPress}
+          onLongPress={onLongPress}
+          key={route.key}
+          style={{
+            height: 50 + bottomInset,
+          }}>
+          <TabBarLabel style={{marginBottom: bottomInset}}>
+            {route.name}
+          </TabBarLabel>
+        </TabBarItem>
+      );
+    });
+  }, [bottomInset, navigation, state.index, state.routes]);
+
+  return <Wrapper>{tabBarItems}</Wrapper>;
 };
 
 const Wrapper = styled.View`
