1023ca2e095d47c0fe72c87a73cdc6f43dd467f8
yungblud <killi8n@gmail.com>
2023-09-19 22:05:59 +0900

    refactor: refactored useIsSplashReady

--- src/screens/SplashScreen/SplashScreen.tsx
+++ src/screens/SplashScreen/SplashScreen.tsx
@@ -1,30 +1,12 @@
-import {useLayoutEffect} from 'react';
 import {View} from 'react-native';
-import {useIsSplashReady} from '../../lib/hooks/useIsSplashReady';
-import {useSplashScreenNavigation} from './hooks';
+import {useSplashTimeout} from './hooks';
 
 interface SplashScreenProps {}
 
 const SplashScreen = ({}: SplashScreenProps) => {
-  const navigation = useSplashScreenNavigation();
-  const [, setIsReady] = useIsSplashReady();
-
-  useLayoutEffect(() => {
-    const subscriber = navigation.addListener('blur', () => {
-      setIsReady(true);
-    });
-    const timeoutId = setTimeout(() => {
-      navigation.navigate('MainBottomTab', {
-        screen: 'HomeStack',
-        params: {},
-      });
-    }, 1500);
-
-    return () => {
-      subscriber();
-      clearTimeout(timeoutId);
-    };
-  }, [navigation, setIsReady]);
+  useSplashTimeout({
+    timeoutDuration: 1500,
+  });
 
   return <View style={{flex: 1, backgroundColor: 'blue'}} />;
 };
--- src/screens/SplashScreen/hooks.ts
+++ src/screens/SplashScreen/hooks.ts
@@ -1,4 +1,6 @@
+import {useIsSplashReady} from '@app/lib/hooks/useIsSplashReady';
 import {useNavigation, useRoute} from '@react-navigation/native';
+import {useCallback, useLayoutEffect} from 'react';
 
 import type {SplashScreenScreenProps} from './types';
 
@@ -6,3 +8,31 @@ export const useSplashScreenNavigation = () =>
   useNavigation<SplashScreenScreenProps['navigation']>();
 export const useSplashScreenRoute = () =>
   useRoute<SplashScreenScreenProps['route']>();
+
+export const useSplashTimeout = ({
+  timeoutDuration,
+}: {
+  timeoutDuration: number;
+}) => {
+  const navigation = useSplashScreenNavigation();
+  const [, setIsSplashReady] = useIsSplashReady();
+
+  const navigateToHome = useCallback(() => {
+    navigation.navigate('MainBottomTab', {
+      screen: 'HomeStack',
+      params: {},
+    });
+  }, [navigation]);
+
+  useLayoutEffect(() => {
+    const unsubscribe = navigation.addListener('blur', () => {
+      setIsSplashReady(true);
+    });
+    const timeoutId = setTimeout(navigateToHome, timeoutDuration);
+
+    return () => {
+      unsubscribe();
+      clearTimeout(timeoutId);
+    };
+  }, [navigateToHome, navigation, setIsSplashReady, timeoutDuration]);
+};
